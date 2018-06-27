c43c0b0f567f17e24426519e65dd1ba9b7a8c2b7
liubin <liumeile0608@163.com>
2018-06-27 15:20:12 +0800

    234

--- core/活动排序.py
+++ core/活动排序.py
@@ -19,17 +19,19 @@ import json
 import operator
 
 
-class TestManSong(unittest.TestCase):
+class Paixu(unittest.TestCase):
 
     """
-    产品8 2+
-    赠品7 数量1
+    
+    6472,6473, 6468, 6469, 6467, 6471, 6475, 6474, 6470, 6470, 6470, 6477, 6476
     """
     def setUp(self):
         self.url = 'http://test.wgmf.com/promotion-online/online/validActivity'
 
     def test_get_activity(self):
 
+
+        #满减
         post_data = {
             "activityId": 0,
             "cusId": "16788112",
@@ -48,6 +50,17 @@ class TestManSong(unittest.TestCase):
                         "templateId": "9",
                         "wareHouseCode": "666"
                     },
+                    {
+                        "code": "7",
+                        "name": "百年老树鲜核桃7",
+                        "num": "2",
+                        "price": 5,
+                        "proImage": "string",
+                        "skuName": "2斤装",
+                        "specialPrice": 0,
+                        "templateId": "7",
+                        "wareHouseCode": "888"
+                    }
 
                 ]
             },
@@ -57,21 +70,73 @@ class TestManSong(unittest.TestCase):
         res = requests.post(url=self.url, json=post_data)
         res_dict = json.loads(res.text)
 
-        print(res_dict)
+        activity_list = []
+        for acticity in res_dict['resultInfo']:
+            activity_list.append(acticity['activityInfo']['activityId'])
 
-        # #活动ID
-        # activityId = res_dict['resultInfo'][0]['activityInfo']['activityId']
-        # #赠品ID
-        # giftcode = res_dict['resultInfo'][0]['giftRuleResultContext']['giftProductInfo'][0]['code']
-        # #赠品数量
-        # giftnum = res_dict['resultInfo'][0]['giftRuleResultContext']['giftProductInfo'][0]['num']
+
+
+        #活动ID
+        # activityId_1 = res_dict['resultInfo'][0]['activityInfo']['activityId']
+        # activityId_2 = res_dict['resultInfo'][1]['activityInfo']['activityId']
+
         #
-        # self.assertEqual(activityId,6466, msg="结果不匹配")
-        # self.assertEqual(giftcode,'7',msg="赠品ID=5")
-        # self.assertEqual(giftnum,'1',msg='赠品数量为1')
+        # self.assertEqual(activityId_1,6468, msg="结果不匹配")
+        # self.assertEqual(activityId_2,6469, msg="结果不匹配")
+
+        #self.assertEqual(activity_list,[6472,6473, 6468, 6469, 6467, 6471, 6475, 6474, 6470, 6470, 6470, 6477, 6476])
 
 
 
+        #满赠
+
+
+
+        post_data = {
+            "activityId": 0,
+            "cusId": "16788112",
+            "exchangeCode": "",
+            "openId": "",
+            "orderContext": {
+                "proList": [
+                    {
+                        "code": "8",
+                        "id": 8,
+                        "name": "桃仁阿胶糕8",
+                        "num": "6",
+                        "price": 9,
+                        "proImage": "string",
+                        "skuName": "桃仁阿胶糕8",
+                        "specialPrice": 0,
+                        "templateId": "8",
+                        "wareHouseCode": "555"
+                    }
+
+                ]
+            },
+            "phone": "string"
+        }
+
+        res = requests.post(url=self.url, json=post_data)
+        res_dict = json.loads(res.text)
+
+        activity_list = []
+        for acticity in res_dict['resultInfo']:
+
+
+            activity_list.append(acticity['activityInfo']['activityId'])
+
+        print(activity_list)
+
+        # 活动ID
+        # activityId_1 = res_dict['resultInfo'][0]['activityInfo']['activityId']
+        # activityId_2 = res_dict['resultInfo'][1]['activityInfo']['activityId']
+
+        #
+        # self.assertEqual(activityId_1,6468, msg="结果不匹配")
+        # self.assertEqual(activityId_2,6469, msg="结果不匹配")
+
+        #self.assertEqual(activity_list, [6472, 6473, 6468, 6469, 6467, 6471, 6475, 6474, 6470, 6470, 6470, 6477, 6476])
 
     def tearDown(self):
         pass
